437729a39a6a4e8c331a5cfe145d43973e8bc5d8
tpham93 <tuanphamminh93@gmail.com>
2022-12-07 20:59:08 +0000

    Deploying to gh-pages from @ tpham93/ChimneyClimber@a6ea41d5a57abf0f1dd5ceb722dcdedb5a5898f6 🚀

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-ChimneyClimber-0.0.10";
+const cacheName = "DefaultCompany-ChimneyClimber-0.0.12";
 const contentToCache = [
     "Build/WebGL.loader.js",
-    "Build/WebGL.framework.js.gz",
-    "Build/WebGL.data.gz",
-    "Build/WebGL.wasm.gz",
+    "Build/WebGL.framework.js",
+    "Build/WebGL.data",
+    "Build/WebGL.wasm",
     "TemplateData/style.css"
 
 ];
